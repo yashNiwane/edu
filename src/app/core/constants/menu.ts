af00ab34a65fdd99dfe6077fc151aee02ee9648e
Vikram Ghadge <vikrambghadge@gmail.com>
2023-08-26 18:47:21 +0530

    home page update sidebare

--- src/app/core/constants/menu.ts
+++ src/app/core/constants/menu.ts
@@ -3,14 +3,13 @@ import { MenuItem } from '../models/menu.model';
 export class Menu {
   public static pages: MenuItem[] = [
     {
-      group: 'Base',
-      separator: false,
+      group: 'July',
+      separator: true,
       items: [
         {
           icon: 'assets/icons/outline/chart-pie.svg',
-          label: 'Dashboard',
-          route: '/dashboard',
-          children: [{ label: 'Nfts', route: '/dashboard/nfts' }],
+          label: 'Dashboard Dashboard Dashboard',
+          route: '/dashboard/nfts',
         },
         {
           icon: 'assets/icons/outline/lock-closed.svg',
@@ -27,13 +26,13 @@ export class Menu {
       ],
     },
     {
-      group: 'Collaboration',
+      group: 'Aug',
       separator: true,
       items: [
         {
           icon: 'assets/icons/outline/download.svg',
           label: 'Download',
-          route: '/download',
+          route: '/downloads',
         },
         {
           icon: 'assets/icons/outline/gift.svg',
@@ -48,8 +47,83 @@ export class Menu {
       ],
     },
     {
-      group: 'Config',
-      separator: false,
+      group: 'Sup',
+      separator: true,
+      items: [
+        {
+          icon: 'assets/icons/outline/cog.svg',
+          label: 'Settings',
+          route: '/settings',
+        },
+        {
+          icon: 'assets/icons/outline/bell.svg',
+          label: 'Notifications',
+          route: '/gift',
+        },
+        {
+          icon: 'assets/icons/outline/folder.svg',
+          label: 'Folders',
+          route: '/folders',
+          children: [
+            { label: 'Current Files', route: '/folders/current-files' },
+            { label: 'Downloads', route: '/folders/download' },
+            { label: 'Trash', route: '/folders/trash' },
+          ],
+        },
+      ],
+    },{
+      group: 'Sup',
+      separator: true,
+      items: [
+        {
+          icon: 'assets/icons/outline/cog.svg',
+          label: 'Settings',
+          route: '/settings',
+        },
+        {
+          icon: 'assets/icons/outline/bell.svg',
+          label: 'Notifications',
+          route: '/gift',
+        },
+        {
+          icon: 'assets/icons/outline/folder.svg',
+          label: 'Folders',
+          route: '/folders',
+          children: [
+            { label: 'Current Files', route: '/folders/current-files' },
+            { label: 'Downloads', route: '/folders/download' },
+            { label: 'Trash', route: '/folders/trash' },
+          ],
+        },
+      ],
+    },{
+      group: 'Sup',
+      separator: true,
+      items: [
+        {
+          icon: 'assets/icons/outline/cog.svg',
+          label: 'Settings',
+          route: '/settings',
+        },
+        {
+          icon: 'assets/icons/outline/bell.svg',
+          label: 'Notifications',
+          route: '/gift',
+        },
+        {
+          icon: 'assets/icons/outline/folder.svg',
+          label: 'Folders',
+          route: '/folders',
+          children: [
+            { label: 'Current Files', route: '/folders/current-files' },
+            { label: 'Downloads', route: '/folders/download' },
+            { label: 'Trash', route: '/folders/trash' },
+          ],
+        },
+      ],
+    },{
+      group: 'Sup',
+      separator: true,
       items: [
         {
           icon: 'assets/icons/outline/cog.svg',
